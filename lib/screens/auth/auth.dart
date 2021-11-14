17c058e16913e2f90bed96c8b1229a323c295997
ranajahanzaib <iam@ranajahanzaib.com>
2021-11-14 05:32:00 +0500

    .

---
+++ lib/screens/auth/auth.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Auth Screen'),
+    );
+  }
+}
---
+++ lib/screens/auth_wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:brew_crew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Return either Home or Auth widget
+    return HomeScreen();
+  }
+}
